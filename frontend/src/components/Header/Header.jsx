35ec7e1a27bebced480246c2316f53eb861a8e8c
Rishikesh-Nalamasu <rishikeshnalamasu@gmail.com>
2026-01-30 05:25:24 +0530

    version 2

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import './Header.css'
 const Header = () => {
   return (
-    <div className = 'Header'>
-      <div className="Header-Contents">
+    <div className = 'header'>
+      <div className="header-contents">
         <h2>Order Yout Favorite Food here</h2>
+        <p>Description dddddddddddddddddddddddddddd</p>
+        <button>View Menu</button>
+
       </div>
     </div>
   )
